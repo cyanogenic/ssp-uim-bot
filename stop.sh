b01fd7d1853ebe47050c20a48420cde2693fdaf2
cyanogenic <zhouziqi@ityut.com>
2021-03-01 23:52:50 +0800

    update

--- stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo -ne "stoping bot..."
-ps -ef | grep "app.py" | grep -v "grep" | awk '{print $2}' | xargs -n1 kill > /dev/null 2>&1
-echo "OK"
-echo -ne "stoping frp..."
-ps -ef | grep "frp" | grep -v "grep" | awk '{print $2}' | xargs -n1 kill > /dev/null 2>&1
-echo "OK"
